cfd3f3c9fb31068ffa29ef0ad14616c4bb54f7c6
RushilSavalia01 <rsavalia@umd.edu>
2022-03-04 14:28:47 -0500

    Test

--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -1,10 +1,20 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
+
+import javax.tools.DocumentationTool.Location;
 
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+			NoFrame frame = new NoFrame();
+			Ghost gh = frame.addGhost(new Location(9,11), "Hi", Color.BLACK);
+			Map map = frame.getMap();
+			ArrayList<Location> s = new ArrayList();
+			s.add(new Location(9,12));
+			s.add(new Location(10,11));
+			s.add(new Location(10,12));
+			assertTrue(gh.get_valid_moves().equals(s));
 	}
 }
