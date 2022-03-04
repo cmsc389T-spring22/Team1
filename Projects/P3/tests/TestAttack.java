6d2d372d7a7f5ee8572e211c7807c662ef82f163
vrundajasani <vrundajsani@gmail.com>
2022-03-04 12:09:41 -0500

    fixed errors

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -7,7 +7,7 @@ public class TestAttack extends TestCase {
 	public void testAttack() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
 		Ghost gh = frame.addGhost(new Location(9,11), "Ghost", Color.red);
-		PacMAn pac = frame.addPacMan(new Location(9,12));
+		PacMan pac = frame.addPacMan(new Location(9,12));
 		assertTrue(ghost.attack());
 	}
 }
