a7aa8c4006897719a83e96d43dbef517f33d144b
FIRSSST <78461888+FIRSSST@users.noreply.github.com>
2022-03-03 23:35:14 -0500

    added tests get_loc

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -6,6 +6,9 @@ import java.io.*;
 public class TestMapGetLoc {
 	
 	public void testMapGetLoc() {
-		return null;
+		NoFrame frame = new NoFrame();
+		PacMan pac = frame.addPacMan(new Location(9,11));
+		Map map = frame.getMap();
+		assertTrue(map.getLoc(new Location(9,11)) == Map.Type.PACMAN);
 	}
 }
