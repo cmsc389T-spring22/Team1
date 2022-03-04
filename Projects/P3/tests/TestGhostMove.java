96e8631de17527e51e2af28dd241b55afcfc163f
FIRSSST <78461888+FIRSSST@users.noreply.github.com>
2022-03-03 23:38:00 -0500

    added tests

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -5,6 +5,8 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		NoFrame frame2 = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(9,12), "g1", Color.black);
+		assertTrue(ghost.move() == true);
 	}
 }
