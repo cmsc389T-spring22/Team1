ed8e7a519fdf7d3295c2fbfcdd2b8292c94b3395
RushilSavalia01 <rsavalia@umd.edu>
2022-03-04 19:49:31 -0500

    Added tests

--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -1,10 +1,15 @@
+
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
-
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		Ghost gh = frame.addGhost(new Location(9,11), "Hi", Color.BLACK);
+		Map map = frame.getMap();
+		ArrayList<Location> s = new ArrayList();
+		assertTrue(gh.get_valid_moves().contains(new Location(9,12)));
+		assertTrue(gh.get_valid_moves().contains(new Location(10,11)));
 	}
 }
--- Projects/P3/tests/TestPacManValidMoves.java
+++ Projects/P3/tests/TestPacManValidMoves.java
@@ -2,9 +2,15 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
+
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		Pacman gh = frame.addPacman(new Location(9,11));
+		Map map = frame.getMap();
+		ArrayList<Location> s = new ArrayList();
+		assertTrue(gh.get_valid_moves().contains(new Location(9,12)));
+		assertTrue(gh.get_valid_moves().contains(new Location(10,11)));
 	}
 }
