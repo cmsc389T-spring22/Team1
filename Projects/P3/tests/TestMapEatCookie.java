067be327cbcbd9b5d1e6665ab7fa08b78d502b3d
Vrunda Jasani <98854578+vrundajasani@users.noreply.github.com>
2022-03-15 17:27:57 -0400

    Update TestMapEatCookie.java

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -3,13 +3,12 @@ import java.awt.Color;
 import java.io.*;
 import java.util.Map;
 
-public class TestMapEatCookie {
-
-	public void testMapEatCookie() {
+public class TestMapEatCookie extends TestCase{
+	
+	public void testMapEatCookie() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
 		Map m = frame.getMap();
-		frame.addPacMan(new Location(4, 6));
-		assertTrue(m.eatCookie("pacman") == null);
-
+		frame.addPacMan(new Location(4,6));
+		assertTrue(m.eatCookie("pacman") != null);
 	}
 }
