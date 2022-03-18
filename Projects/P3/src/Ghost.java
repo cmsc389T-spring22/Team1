e3764e716cdc3a50037371df10f433f3b96770ec
Selena Xiao <sxiao123@umd.edu>
2022-03-18 18:17:12 -0400

    Fixed - Selena

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -42,8 +42,8 @@ public class Ghost{
 		if (possibleLocations.size() != 0) {
 			int randomIndx = 0 + (int)(Math.random() * (((possibleLocations.size()-1) - 0) + 1));
 			myLoc = new Location(possibleLocations.get(randomIndx).x, possibleLocations.get(randomIndx).y);
-			myMap.move(myName, new Location(myLoc.x+69999, myLoc.y+69999), Map.Type.GHOST);
-			return false;
+			myMap.move(myName, new Location(myLoc.x, myLoc.y), Map.Type.GHOST);
+			return true;
 		}
 		return false;
 	}
