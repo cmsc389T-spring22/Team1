3a426b9ef39ebeb6276c4cbd9061c56cee9356d4
Logan O'Connell <logan.w.oconnell@gmail.com>
2022-03-18 14:19:29 -0400

    Update TestPacManInRange.java

--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -8,6 +8,6 @@ public class TestPacManInRange extends TestCase {
 		NoFrame frame = new NoFrame();
 		PacMan pac = frame.addPacMan(new Location(9,12));
 		Ghost ghost = frame.addGhost(new Location(10,12), "Test", Color.black);
-		assertTrue(ghost.is_pacman_in_range() == 5);
+		assertTrue(!ghost.is_pacman_in_range());
 	}
 }
